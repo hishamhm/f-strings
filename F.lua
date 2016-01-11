f2e8c87f72bfe6ea27f2e65a8396e588360f94df
Hisham Muhammad <hisham@gobolinux.org>
2016-01-11 02:03:54 -0200

    Handle every case except unreferenced upvalues.

--- F.lua
+++ F.lua
@@ -14,25 +14,31 @@ if _VERSION == "Lua 5.1" then
    end
 end
 
+local function scan_using(scanner, arg, searched)
+   local i = 1
+   repeat
+      local name, value = scanner(arg, i)
+      if name == searched then
+         return true, value
+      end
+      i = i + 1
+   until name == nil
+   return false
+end
+
+local function snd(_, b) return b end
+
 local function format(_, str)
-   local outer_env = _ENV or getfenv(1)
+   local outer_env = _ENV and (snd(scan_using(debug.getlocal, 3, "_ENV")) or snd(scan_using(debug.getupvalue, debug.getinfo(2, "f").func, "_ENV")) or _ENV) or getfenv(2)
    return (str:gsub("%b{}", function(block)
       local code, fmt = block:match("{(.*):(%%.*)}")
       code = code or block:match("{(.*)}")
       local exp_env = {}
       setmetatable(exp_env, { __index = function(_, k)
-         local stack_level = 5
-         while debug.getinfo(stack_level, "") ~= nil do
-            local i = 1
-            repeat
-               local name, value = debug.getlocal(stack_level, i)
-               if name == k then
-                  return value
-               end
-               i = i + 1
-            until name == nil
-            stack_level = stack_level + 1
-         end
+         local ok, value = scan_using(debug.getupvalue, debug.getinfo(6, "f").func, k)
+         if ok then return value end
+         ok, value = scan_using(debug.getlocal, 7, k)
+         if ok then return value end
          return rawget(outer_env, k)
       end })
       local fn, err = load("return "..code, "expression `"..code.."`", "t", exp_env)
