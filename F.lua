e3ec69c4e3c083a5ef8a57da89f3390faed92c19
Lee Busby <busby1@llnl.gov>
2019-07-22 14:46:58 -0700

    brute force handling for {{ and }}

--- F.lua
+++ F.lua
@@ -28,9 +28,12 @@ end
 
 local function snd(_, b) return b end
 
+F._ocb, F._ccb = '{', '}'
+
 local function format(_, str)
    local outer_env = _ENV and (snd(scan_using(debug.getlocal, 3, "_ENV")) or snd(scan_using(debug.getupvalue, debug.getinfo(2, "f").func, "_ENV")) or _ENV) or getfenv(2)
-   return (str:gsub("%b{}", function(block)
+   local str1 = string.gsub(str:gsub("}}","{F._ccb}"),"{{","{F._ocb}")
+   return (str1:gsub("%b{}", function(block)
       local code, fmt = block:match("{(.*):(%%.*)}")
       code = code or block:match("{(.*)}")
       local exp_env = {}
